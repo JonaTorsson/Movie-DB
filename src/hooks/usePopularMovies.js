0321eea7f20269700c13e1ed363bc65e43f738e3
JonaTorsson <jona.torsson@gmail.com>
2022-08-22 13:11:33 +0200

    Renamed file to navigate easier

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,8 @@
+import { useQuery } from "react-query";
+import TmDBAPI from "../services/TmDBAPI";
+
+const usePopularMovies = () => {
+    return useQuery('movie-popular', TmDBAPI.getPopularMovies, {keepPreviousData: true})
+}
+
+export default usePopularMovies
