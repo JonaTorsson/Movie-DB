e544c5341c160a2bb731f5aa676c6440d1dbcfc6
JonaTorsson <jona.torsson@gmail.com>
2022-08-22 13:11:06 +0200

    Added getCurrentMovies

--- src/services/TmDBAPI.js
+++ src/services/TmDBAPI.js
@@ -4,11 +4,18 @@ import axios from 'axios'
 const BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
-const getPopular = async () => {
+const getPopularMovies = async () => {
 	const res = await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}&region=US`)
 	return res.data.results
 }
 
+const getCurrentMovies = async () => {
+	const res = await axios.get(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`)
+	return res.data.results
+}
+
 export default {
-	getPopular,
+	getPopularMovies,
+	getCurrentMovies,
+
 }
