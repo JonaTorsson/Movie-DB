d02d51ca5fe6dc92eefd591fbc7821dc819ffe11
JonaTorsson <jona.torsson@gmail.com>
2022-08-23 13:38:50 +0200

    Made cards global

--- src/components/InfoCardMovie.jsx
+++ src/components/InfoCardMovie.jsx
@@ -1,37 +1,34 @@
 import { Col, Row, Card, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import usePopularMovies from "../hooks/usePopularMovies"
 
-const InfoCardMovie = () => {
-	const { data: popularMovies } = usePopularMovies()
-
+const InfoCardMovie = ({ movies }) => {
 	const BASE_URL_IMAGE = 'https://image.tmdb.org/t/p/w500/'
 	
 	return (
 		<Row>
-			<Col sm="12" md="6" lg="4">
-				{popularMovies.map((movie) => (
-					<Card key={movie.id} className="mb-3">
-						<Card.Img 
-							className="" 
-							variant="top" 
-							src={BASE_URL_IMAGE + movie.poster_path}
-						/>
-						<Card.Body>
-							<Card.Title> {movie.original_title} </Card.Title>
-							<Card.Text> 
-									<span className="fw-bold"> Average score: </span>  {movie.vote_average}
-							</Card.Text>
-							<Button 
-								as={Link}
-								to={`/movie/${movie.id}`}
-								variant="info">
-								Read more
-							</Button>
-						</Card.Body>
-					</Card>
-				))}
-			</Col>
+			{movies.map(movie => 
+			<Col key={movie.id} xs={12} lg={6} xl={4}>
+				<Card key={movie.id} className="mb-3">
+					<Card.Img 
+						className="" 
+						variant="top" 
+						src={BASE_URL_IMAGE + movie.poster_path}
+					/>
+					<Card.Body>
+						<Card.Title> {movie.title} </Card.Title>
+						<Card.Text> 
+								<span className="fw-bold"> Average score: </span>  {movie.vote_average}
+						</Card.Text>
+						<Button 
+							as={Link}
+							to={`/movie/${movie.id}`}
+							variant="info">
+							Read more
+						</Button>
+					</Card.Body>
+				</Card>
+				</Col>
+			)}
 		</Row>
 	)
 }
