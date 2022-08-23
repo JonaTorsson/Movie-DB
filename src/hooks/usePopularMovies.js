77dad3237cb64cc9bee38bcfbd1abec74bba9195
JonaTorsson <jona.torsson@gmail.com>
2022-08-23 13:36:38 +0200

    small changes

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query"
 import TmDBAPI from "../services/TmDBAPI"
 
 const usePopularMovies = () => {
-    return useQuery('movie-popular', TmDBAPI.getPopularMovies, {keepPreviousData: true})
+    return useQuery('popularMovies', TmDBAPI.getPopularMovies)
 }
 
 export default usePopularMovies
