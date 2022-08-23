fcb965927581c8b291948abb52af99fafea23d53
JonaTorsson <jona.torsson@gmail.com>
2022-08-23 13:37:21 +0200

    added route for current movies

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage'
 import NotFound from './pages/NotFound'
 import './assets/scss/App.scss'
 import PopularMovies from './pages/PopularMovies'
+import CurrentMoviesPage from './pages/CurrentMoviesPage'
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="*" element={<NotFound />} />
 				<Route path="/popular" element={<PopularMovies />} />
+				<Route path="/current" element={<CurrentMoviesPage />} />
 			</Routes>
 		</div>
 	)
