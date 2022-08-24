38b161e883683904ea5bef444de070a66f09f143
JonaTorsson <jona.torsson@gmail.com>
2022-08-24 10:46:12 +0200

    hook for top rated movies

---
+++ src/hooks/useTopRatedMovies.js
@@ -0,0 +1,9 @@
+import { useQuery } from 'react-query'
+import TmDBAPI from "../services/TmDBAPI"
+
+
+const useTopRatedMovies = () => {
+	return useQuery('topRated', TmDBAPI.getTopRatedMovies)
+}
+
+export default useTopRatedMovies
