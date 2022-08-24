2c7e19741f2271abd0de9b6a88f02a1501052d31
JonaTorsson <jona.torsson@gmail.com>
2022-08-24 10:44:52 +0200

    Added getmovie and get actor

--- src/services/TmDBAPI.js
+++ src/services/TmDBAPI.js
@@ -5,17 +5,31 @@ const BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 const getPopularMovies = async () => {
-	const res = await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}&region=US`)
-	return res.data.results
+	return await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}&region=US`)
 }
 
 const getCurrentMovies = async () => {
-	const res = await axios.get(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`)
-	return res.data.results
+	return await axios.get(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`)
+}
+
+const getTopRatedMovies = async () => {
+	return await axios.get(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}`)
+}
+
+const getActor = async (id) => {
+	return await axios.get(`${BASE_URL}/person/${id}?api_key=${API_KEY}`)
+}
+
+const getMovie = async (id) => {
+	 return await axios.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=credits,similar`)
+
 }
 
 export default {
 	getPopularMovies,
 	getCurrentMovies,
+	getTopRatedMovies,
+	getActor,
+	getMovie,
 
 }
