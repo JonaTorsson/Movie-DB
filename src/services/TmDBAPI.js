9c819ec38267ad51165cc94952776360aa16fb9c
JonaTorsson <jona.torsson@gmail.com>
2022-08-29 20:26:00 +0200

    Small changes and added getGenre

--- src/services/TmDBAPI.js
+++ src/services/TmDBAPI.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 const getPopularMovies = async () => {
-	return await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}&region=US`)
+	return await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
 }
 
 const getCurrentMovies = async () => {
@@ -17,12 +17,22 @@ const getTopRatedMovies = async () => {
 }
 
 const getActor = async (id) => {
-	return await axios.get(`${BASE_URL}/person/${id}?api_key=${API_KEY}`)
+	return await axios.get(`${BASE_URL}/person/${id}?api_key=${API_KEY}&append_to_response=combined_credits`)
 }
 
 const getMovie = async (id) => {
 	 return await axios.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=credits,similar`)
+}
 
+const getGenre = async ({queryKey}) => {
+	const [_key, { page, genre_id }] = queryKey
+	const res = await axios.get(`${BASE_URL}/discover/movie/?api_key=${API_KEY}&with_genres=${genre_id}&include_adult=false&page=${page}`)
+	return res.data
+}
+
+const getGenres = async () => {
+	const res = await axios.get(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`)
+	return res.data
 }
 
 export default {
@@ -31,5 +41,6 @@ export default {
 	getTopRatedMovies,
 	getActor,
 	getMovie,
-
+	getGenres,
+	getGenre,
 }
